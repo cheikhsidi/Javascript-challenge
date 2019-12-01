6245dac2118e14c07cee65582f79afe4d2749db6
Cheikh Sidi <14164306+cheikhsidi@users.noreply.github.com>
2019-12-01 14:23:01 -0500

    modified js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,11 @@
 // from data.js
 var tableData = data;
 
-// Creating a function to load the data!
+// Creating a function to load the data into my html table!
 function loadData(data) {
     var tbody = d3.select('tbody')
     tbody.text('');
-    console.log(tbody)
+    // console.log(tbody)
     data.forEach(element => {
         row = tbody.append('tr')
         Object.entries(element).forEach(([key, value]) => {
@@ -35,3 +35,40 @@ button.on('click', function() {
         loadData(filteredData);
     };
 });
+
+
+// ------------------------------------------------------------------------------------
+
+// // another way to add data and filter 
+// var renderDate = (newdata) => {
+//     tbody = d3.select('tbody');
+//     newdata.forEach(element => {
+//         var rows = tbody.selectAll("tr").data(newdata);
+//         rows.enter()
+//             .append('tr')
+//             .selectAll("td")
+//             .data(function(d) { return [d.datetime, d.city, d.state, d.country, d.shape, d.durationMimutes, d.comments]; })
+//             .enter()
+//             .append("td")
+//             .text(function(d) { return d; });
+
+//         rows.exit().remove();
+
+//     });
+// };
+
+// renderDate(tableData)
+
+// // Another way to add filter event listner
+// button.on('click', function() {
+//     date = inputDate.property('value');
+//     if (!date.length) {
+//         loadData(tableData);
+//     } else {
+//         console.log(date);
+//         var filteredData = tableData.filter(function(d) { return d.datetime == date; });
+//         console.log(filteredData);
+//         // loadData(filteredData);
+//         renderDate(filteredData)
+//     };
+// });
